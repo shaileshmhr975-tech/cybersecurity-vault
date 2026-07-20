b6496b472ba3451d5e1ea2f859d0a9cd90e2eb62
Shailesh Mahar <shaileshmhr975@gmail.com>
2026-07-20 16:03:37 +0530

    continue

--- sysadmin-scripts/Employees.py
+++ sysadmin-scripts/Employees.py
@@ -1,13 +1,13 @@
 class Employees:
-	def _init_(self, name, department, role, salary, year_employed):
+	def __init__(self, name, department, role, salary, years_employed):
 		self.name = name
 		self.department = department
+		self.role = role
 		self.salary = salary
-		self.year = year
-		self.year_employed = year_employed	
+		self.years_employed = years_employed	
 	
 	def eligible_for_retirement(self):
-		if self.year_employed >20:
+		if self.years_employed >=20:
 			return True
 		else :
 			return False
